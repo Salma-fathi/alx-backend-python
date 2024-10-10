59fe91f32125e87b4af2fc31eda5f4426e4af31a
Salma-fathi <salma89040@gmail.com>
2024-10-10 10:28:59 +0400

    add new repo and also add project variable annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Add two numbers"""
+def add(a : float, b : float) -> float:
+    """My addition function
+
+    Args:
+        a: first float
+        b: second float
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Concatenate two strings"""
+
+def concat(str1 : str, str2 : str) -> str:
+    """My addition function
+
+    Args:
+        str1: first string
+        str2: second string
+
+    Returns:
+        The return value. str1 + str2
+    """
+    return (str1 + str2)
