bf1f2e942d10e8434a1bf79a48c5e73547a401aa
Salma-fathi <salma89040@gmail.com>
2024-10-11 06:29:29 +0400

    make file executablee

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -3,11 +3,18 @@
 '''
 from typing import Any, Sequence, Union
 
+from typing import Sequence, Union, Any, TypeVar, Mapping
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    '''Retrieves the first element of a sequence if it exists.
-    '''
-    if lst:
-        return lst[0]
+R = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[R, None] = None) -> Union[Any, R]:
+    """
+    Add type annotations to function
+    safely_get_value
+    """
+    if key in dct:
+        return dct[key]
     else:
-        return None
+        return default
