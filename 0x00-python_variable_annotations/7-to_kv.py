784c58393680d3c0b848d7d5e3c94ea268781abb
Salma-fathi <salma89040@gmail.com>
2024-10-11 06:22:23 +0400

    fixed error in tasks

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python3
-"""
-# The code snippet you provided is defining a Python function called `to_kv` that takes a string `k` and an integer or float `v` as input arguments. The function is annotated with type hints using the `typing` module to specify the types of the input arguments and the return value.
-Complex types - string and int/float to tuple
-Write typed-annotated function to_kv that takes str k and an int OR float v
-Returns a tuple
-"""
+'''Task 7
+'''
 from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """
-    Typed-annotated function
-    to_kv
-    """
-    return (k, v * v)
+    '''Converts a key and its value to a tuple of the key and
+    the square of its value.
+    '''
+    return (k, float(v**2))
