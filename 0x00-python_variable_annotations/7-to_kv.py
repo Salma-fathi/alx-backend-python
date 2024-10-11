df812c5c8bd74167dd062ac5af1888bf979e2326
Salma-fathi <salma89040@gmail.com>
2024-10-11 06:20:35 +0400

    fixed error in task 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """
+# The code snippet you provided is defining a Python function called `to_kv` that takes a string `k` and an integer or float `v` as input arguments. The function is annotated with type hints using the `typing` module to specify the types of the input arguments and the return value.
 Complex types - string and int/float to tuple
 Write typed-annotated function to_kv that takes str k and an int OR float v
 Returns a tuple
@@ -13,4 +14,3 @@ def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     to_kv
     """
     return (k, v * v)
-
