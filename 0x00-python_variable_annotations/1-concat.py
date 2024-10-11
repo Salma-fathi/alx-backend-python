442a0f1865e4fdce4a2e9f39f73ca7dfc820e27f
Salma-fathi <salma89040@gmail.com>
2024-10-11 06:17:32 +0400

    fixed error in task 1

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,15 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+''' task 1.
+'''
 
-"""Concatenate two strings"""
 
-def concat(str1 : str, str2 : str) -> str:
-    """My addition function
-
-    Args:
-        str1: first string
-        str2: second string
-
-    Returns:
-        The return value. str1 + str2
-    """
-    return (str1 + str2)
+def concat(str1: str, str2: str) -> str:
+    '''Concatenates two strings.
+    '''
+    return str1 + str2
