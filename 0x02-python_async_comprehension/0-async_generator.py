96fc992e383a52adf8e5aa9819d1b46e10d44a8f
Salma-fathi <salma89040@gmail.com>
2024-10-15 13:09:40 +0400

    new udate in task 1

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,11 @@
 """0. Async Generator"""
 import asyncio
 import random
+import typing 
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """Async Generator"""
     for _ in range(10):
         await asyncio.sleep(1)
