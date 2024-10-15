b34425dc7d5b4509d2dd2347184db882815bf7ee
Salma-fathi <salma89040@gmail.com>
2024-10-15 13:57:32 +0400

    add taks 1,2

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,10 +3,8 @@
 import asyncio
 import random
 import typing 
-from typing import Generator
 
-
-async def async_generator() -> Generator[float, None, None]:
+async def async_generator() -> typing.AsyncGenerator[float, None]:
     """Async Generator"""
     for _ in range(10):
         await asyncio.sleep(1)
